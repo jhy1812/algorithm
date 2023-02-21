905f3d266349bd96e2060af27202ace6c71ec8f3
jhy1812 <122588619+jhy1812@users.noreply.github.com>
2023-02-22 02:17:23 +0900

    [Bronze I] Title: 색종이, Time: 3064 ms, Memory: 123148 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr = [[0]*1001 for _ in range(1001)]
+arr = [[0 for _ in range(1001)] for _ in range(1001)]
 
 for i in range(N):
     x1, y1, m, n = [*map(int, input().split())]
@@ -10,10 +10,10 @@ for i in range(N):
         for k in range(y1, y1+n):
             arr[k][j] = i + 1
 
-bucket = [0]*(N+1)
-
-for j in range(1001):
-    for k in range(1001):
-        bucket[arr[j][k]] += 1
-for i in range(1, N+1):
-    print(bucket[i])
+for i in range(N):
+    cnt = 0
+    for j in range(1001):
+        for k in range(1001):
+            if arr[j][k] == i+1:
+                cnt += 1
+    print(cnt)
