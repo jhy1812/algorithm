fe5b873a892f95830ee0ea92008e94ad9a81566f
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-03-27 15:54:53 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long a, b, c;
+	scanf("%lld %lld %lld", &a, &b, &c);
+	printf("%lld", a + b + c);
+	return 0;
+}
