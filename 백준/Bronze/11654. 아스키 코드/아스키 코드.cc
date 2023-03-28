4ca2da2afb1abf4a3ee846c0fe1df8bb0b5217a7
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-03-28 17:12:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	char s;
+	cin >> s;
+	int i = int(s);
+	cout << i;
+	return 0;
+}
