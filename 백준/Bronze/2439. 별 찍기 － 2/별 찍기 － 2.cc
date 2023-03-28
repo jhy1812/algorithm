53f41290fc52008d5137a0425c085dae571e153d
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-03-28 09:44:24 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#define _CRT_SECURE_NO_WARNINGS 
+using namespace std;
+
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = 1; i < N+1; i++) {
+		for (int j = 0; j < N - i; j++) {
+			printf(" ");
+		}
+		for (int j = 0; j < i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
