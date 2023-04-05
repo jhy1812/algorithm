a1697cc4e77a6902c2cbe5f103ee8a0b11a37004
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-04-05 10:14:29 +0900

    [Platinum V] Title: 행성 터널, Time: 104 ms, Memory: 15352 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.cc
+++ 백준/Platinum/2887. 행성 터널/행성 터널.cc
@@ -4,54 +4,54 @@
 
 using namespace std;
 
-struct point {
+struct point {			// 행성 좌표 입력할 구조체
 	int x;
 	int y;
 	int z;
 	int num;
 };
 
-struct tunnel {
+struct tunnel {			// 행성 간의 거리 구할 구조체
 	int num1;
 	int num2;
 	int leng;
 };
 
-int N, a, b, c, cnt, summ;
-int boss[100000];
+int N, a, b, c, cnt, summ;		// N - 행성 수, (a, b, c) - 행성 좌표 받을 변수, cnt - 연결 수, summ - 터널 길이 합
+int boss[100000];				// 묶여있는 행성 번호 표시할 배열
 
-bool cmpx(point a, point b) {
+bool cmpx(point a, point b) {	// x 좌표 기준 오름차순 정렬해줄 함수
 	return a.x < b.x;
 }
 
-bool cmpy(point a, point b) {
+bool cmpy(point a, point b) {	// y 좌표 기준 오름차순 정렬해줄 함수
 	return a.y < b.y;
 }
 
-bool cmpz(point a, point b) {
+bool cmpz(point a, point b) {	// z 좌표 기준 오름차순 정렬해줄 함수
 	return a.z < b.z;
 }
 
-bool cmpl(tunnel a, tunnel b) {
+bool cmpl(tunnel a, tunnel b) {	// 터널 길이 기준 오름차순 정렬해줄 함수
 	return a.leng < b.leng;
 }
 
-int findboss(int a) {
-	if (boss[a] == -1) {
+int findboss(int a) {			// 각 행성이 어떤 행성과 묶여있는지 찾아줄 함수
+	if (boss[a] == -1) {		// 묶인 행성 없으면 본인 반환
 		return a;
 	}
-	int ret = findboss(boss[a]);
+	int ret = findboss(boss[a]);	// 묶인 행성 있으면 해당 행성 찾아서 반환
 	boss[a] = ret;
 	return ret;
 }
 
-void un(int a, int b) {
+void un(int a, int b) {				// 두 행성 묶어줄 함수
 	int fa = findboss(a);
 	int fb = findboss(b);
-	if (fa == fb) {
+	if (fa == fb) {					// 이미 묶여있으면 그냥 리턴
 		return;
 	}
-	boss[fb] = fa;
+	boss[fb] = fa;					// 아니면 묶어줌
 }
 
 int main()
@@ -60,8 +60,8 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	vector<point>arr;
-	vector<tunnel>connect;
+	vector<point>arr;				// 행성 좌표 및 번호 저장할 벡터
+	vector<tunnel>connect;			// 행성간 거리 저장할 벡터
 
 	cin >> N;
 
@@ -79,17 +79,17 @@ int main()
 		arr.push_back(tmp);
 	}
 	
-	sort(arr.begin(), arr.end(), cmpx);
+	sort(arr.begin(), arr.end(), cmpx);			// 행성 x 좌표 기준 오름차순 정렬
 
 	for (int i = 1; i < N; i++) {
 		tunnel tmp;
 		tmp.num1 = arr[i - 1].num;
 		tmp.num2 = arr[i].num;
-		tmp.leng = arr[i].x - arr[i - 1].x;
-		connect.push_back(tmp);
+		tmp.leng = arr[i].x - arr[i - 1].x;	
+		connect.push_back(tmp);					// 정렬 후 가까운 행성끼리의 거리 정보 저장 
 	}
 
-	sort(arr.begin(), arr.end(), cmpy);
+	sort(arr.begin(), arr.end(), cmpy);			// y 좌표 기준 오름차순 정렬 후 위와 동일
 
 	for (int i = 1; i < N; i++) {
 		tunnel tmp;
@@ -99,7 +99,7 @@ int main()
 		connect.push_back(tmp);
 	}
 
-	sort(arr.begin(), arr.end(), cmpz);
+	sort(arr.begin(), arr.end(), cmpz);			// z 좌표 기준 오름차순 정렬 후 위와 동일
 
 	for (int i = 1; i < N; i++) {
 		tunnel tmp;
@@ -108,16 +108,16 @@ int main()
 		tmp.leng = arr[i].z - arr[i - 1].z;
 		connect.push_back(tmp);
 	}
-
-	sort(connect.begin(), connect.end(), cmpl);
+	
+	sort(connect.begin(), connect.end(), cmpl);			// 행성간 거리 기준으로 정렬
 
 	for (int i = 0; i<int(connect.size()); i++) {
-		if (findboss(connect[i].num1) != findboss(connect[i].num2)) {
-			un(connect[i].num1, connect[i].num2);
-			cnt += 1;
-			summ += connect[i].leng;
+		if (findboss(connect[i].num1) != findboss(connect[i].num2)) {		// 두행성 연결되지 않았으면
+			un(connect[i].num1, connect[i].num2);							// 연결
+			cnt += 1;														// 연결 수 +1
+			summ += connect[i].leng;										// 거리합 갱신
 		}
-		if (cnt == N - 1) {
+		if (cnt == N - 1) {													// 연결 수 N-1이면 스탑
 			break;
 		}
 	}
