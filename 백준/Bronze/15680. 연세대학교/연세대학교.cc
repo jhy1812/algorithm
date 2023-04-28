510a16aba4fad8565110b47978b001e7e52f9be8
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-04-28 10:14:59 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+    if(N == 1)
+       cout<<"Leading the Way to the Future"<<endl;
+    else
+        cout<<"YONSEI"<<endl;
+    return 0;
+}
