9f04a90260dce1ec6e6c11fc046dda4066454993
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-05-15 14:48:19 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String name;
+		
+		name = scan.next();
+		
+		System.out.println(name + "??!");
+	}
+}
