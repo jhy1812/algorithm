c3eae26c75891e3b3e3c0b06ec488a99c9dd3126
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-05-15 14:37:35 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 17988 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double a, b;
+		
+		a = scan.nextDouble();
+		b = scan.nextDouble();
+		System.out.println(a/b);
+	}
+}
