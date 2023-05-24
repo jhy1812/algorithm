cdf2250cadb3e161888e0a37cc5958ab6bec1368
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-05-24 19:25:18 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int L;
+    std::cin >> L;
+    
+    if (L%5 == 0){
+        std::cout << L/5;
+    }
+    else {
+        std::cout << L/5+1;
+    }
+    return 0;
+}
