3c52489a2f46f449462264e47c3570b4ed3a9ce1
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-06-22 22:08:19 +0900

    [Platinum V] Title: 피보나치 수의 제곱의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Platinum/11440. 피보나치 수의 제곱의 합/피보나치 수의 제곱의 합.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define MOD 1000000007
+
+long long n;
+std::vector<std::vector<long long>>result(2, std::vector<long long>(2));
+std::vector<std::vector<long long>>mat(2, std::vector<long long>(2));
+
+std::vector<std::vector<long long>> mat_mul(std::vector<std::vector<long long>> a1, std::vector<std::vector<long long>> a2) {
+	std::vector<std::vector<long long>>a3(2, std::vector<long long>(2, 0));
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			long long tmp = 0;
+			for (int k = 0; k < 2; k++) {
+				tmp += (a1[i][k] * a2[k][j]) % MOD;
+				tmp = tmp % MOD;
+			}
+			a3[i][j] = tmp;
+		}
+	}
+	return a3;
+}
+
+int main()
+{
+	std::cin >> n;
+
+	result[0][0] = 1;
+	result[1][1] = 1;
+
+	mat[0][0] = 1;
+	mat[1][0] = 1;
+	mat[0][1] = 1;
+
+	while (n > 0) {
+		if (n % 2 == 1) {
+			result = mat_mul(result, mat);
+		}
+		n = n / 2;
+		mat = mat_mul(mat, mat);
+	}
+
+	std::cout << (result[0][0] * result[0][1]) % MOD;
+
+	return 0;
+}
