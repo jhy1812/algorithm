bcabcf200671d73e41245ae82c2c96151d82341b
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-06-24 03:16:50 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -9,12 +9,15 @@ for tc in range(1, T+1):
         sum += i
         bucket[i] += 1
     j = 100
-    avg = sum/N
+    avg = float(sum)/float(N)
     while 1:
         if j > avg:
            cnt += bucket[j]
         else:
             break
         j -= 1
-    print('%.3f' %(round(cnt/N*100, 3)), end='')
+    result = cnt/N*100
+    if (result*10000)%10 >=5:
+        result += 0.0005
+    print('%.3f' %result, end='')
     print('%')
