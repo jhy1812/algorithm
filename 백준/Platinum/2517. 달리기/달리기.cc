d3f70208addb22589c29982d18254a99ca8c433e
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-07-11 13:32:58 +0900

    [Platinum IV] Title: 달리기, Time: 764 ms, Memory: 123032 KB -BaekjoonHub

---
+++ 백준/Platinum/2517. 달리기/달리기.cc
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int N, result;
+std::vector<int> arr(1);
+std::vector<std::vector<int>> segment(2000000, std::vector<int>());
+
+std::vector<int> make_tree(int st, int ed, int idx) {
+	if (st == ed) {
+		segment[idx].push_back(arr[st]);
+		return segment[idx];
+	}
+	int mid = (st + ed) / 2;
+	std::vector<int> left_node = make_tree(st, mid, 2 * idx);
+	std::vector<int> right_node = make_tree(mid + 1, ed, 2 * idx + 1);
+	int i = 0;
+	int j = 0;
+
+	while (true) {
+		if (i >= int(left_node.size()) && j >= int(right_node.size())) {
+			break;
+		}
+		else if (i >= int(left_node.size())) {
+			segment[idx].push_back(right_node[j]);
+			j++;
+		}
+		else if (j >= int(right_node.size())) {
+			segment[idx].push_back(left_node[i]);
+			i++;
+		}
+		else if (left_node[i] >= right_node[j]) {
+			segment[idx].push_back(right_node[j]);
+			j++;
+		}
+		else if (left_node[i] < right_node[j]) {
+			segment[idx].push_back(left_node[i]);
+			i++;
+		}
+	}
+
+	return segment[idx];
+}
+
+void get_result(int st, int ed, int idx, int right, int target) {
+	if (st >= right) {
+		return;
+	}
+	if (ed < right) {
+		result += std::lower_bound(segment[idx].begin(), segment[idx].end(), target) - segment[idx].begin();
+		return;
+	}
+	int mid = (st + ed) / 2;
+	get_result(st, mid, 2 * idx, right, target);
+	get_result(mid + 1, ed, 2 * idx + 1, right, target);
+	return;
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	std::cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		std::cin >> tmp;
+		arr.push_back(tmp);
+	}
+
+	make_tree(1, N, 1);
+	arr[1] = 1;
+
+	for (int i = 2; i <= N; i++) {
+		result = 0;
+		get_result(1, N, 1, i, arr[i]);
+		arr[i] = i - result;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		std::cout << arr[i] << "\n";
+	}
+
+	return 0;
+}
