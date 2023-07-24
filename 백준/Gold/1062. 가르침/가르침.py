1b7edb9e4d45616d46f8abd0ca85ae03ad7200e9
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-07-24 21:24:52 +0900

    [Gold IV] Title: 가르침, Time: 820 ms, Memory: 123372 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.py
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -2,16 +2,30 @@ N, K = [*map(int, input().split())]
 
 arr = []
 check = [0 for i in range(26)]
+word = [0 for i in range(26)]
 result = 0
+base = 0
+
+for i in 'antic':
+    check[ord(i) - ord('a')] = 1
+    word[ord(i) - ord('a')] = 1
 
 for i in range(N):
     tmp = input()
     tmp = tmp[4:-4]
-    arr.append(tmp)
+    flag = 0
+    for j in tmp:
+        if check[ord(j) - ord('a')] == 0:
+            flag = 1
+        word[ord(j) - ord('a')] = 1
+    if flag:
+        arr.append(tmp)
+    else:
+        base += 1
 
 def dfs(lv, st):
     global result
-    if lv == K:
+    if lv == K or lv == k:
         ret = 0
         for i in arr:
             flag = 0
@@ -27,6 +41,8 @@ def dfs(lv, st):
     for i in range(st, 26):
         if check[i] == 1:
             continue
+        if word[i] == 0:
+            continue
         check[i] = 1
         dfs(lv + 1, i + 1)
         check[i] = 0
@@ -35,7 +51,10 @@ if K < 5:
     print(0)
 else:
     K -= 5
-    for i in 'antic':
-        check[ord(i) - ord('a')] = 1
+    k = 0
+    for i in word:
+        if i == 1:
+            k += 1
+    k -= 5
     dfs(0, 1)
-    print(result)
+    print(result + base)
