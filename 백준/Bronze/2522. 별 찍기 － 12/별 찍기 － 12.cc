7733fab1ea532ba05bc9932f8b9a082c956c8c5e
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-08-27 01:23:24 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int n;
+
+int main()
+{
+    std::cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < n -i; j++) {
+            std::cout << " ";
+        }
+        for(int j =0; j < i; j++) {
+            std::cout<<"*";
+        }
+        
+    std::cout << "\n";
+    }
+    
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++ ){
+            std::cout<<" ";
+        }
+        for(int j = 0; j < n -i; j++){
+            std::cout<<"*";
+        }    
+        std::cout << "\n";
+    }
+    
+    return 0;
+}
