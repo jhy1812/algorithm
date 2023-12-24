c607225df57e3ceb6b326b75ee0662bfd3562089
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2023-12-24 22:01:08 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+int N;
+std::priority_queue<int> q;
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	std::cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		std::cin >> tmp;
+		if (tmp == 0) {
+			if (q.size() == 0) {
+				std::cout << 0 << "\n";
+			}
+			else {
+				std::cout << q.top() << "\n";
+				q.pop();
+			}
+		}
+		else {
+			q.push(tmp);
+		}
+	}
+
+	return 0;
+}
