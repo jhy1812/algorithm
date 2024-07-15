6e04d2f32d7083aaf8a3bd007ffe1d46057774ab
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2024-07-15 18:06:28 +0900

    [Silver III] Title: 1로 만들기, Time: 152 ms, Memory: 18168 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[X + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[X] = 0;
+
+        for (int i = X; i >= 1; i--) {
+            if (i % 3 == 0) {
+                dp[i / 3] = Math.min(dp[i / 3], dp[i] + 1);
+            }
+            if (i % 2 == 0) {
+                dp[i / 2] = Math.min(dp[i / 2], dp[i] + 1);
+            }
+            dp[i - 1] = Math.min(dp[i - 1], dp[i] + 1);
+        }
+
+        System.out.print(dp[1]);
+    }
+}
