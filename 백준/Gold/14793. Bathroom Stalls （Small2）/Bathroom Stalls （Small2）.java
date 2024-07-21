afa547f995b12645e879f3d969eef79b9bd877e1
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2024-07-21 23:35:46 +0900

    [Gold IV] Title: Bathroom Stalls (Small2), Time: 168 ms, Memory: 16176 KB -BaekjoonHub

--- 백준/Gold/14793. Bathroom Stalls （Small2）/Bathroom Stalls （Small2）.java
+++ 백준/Gold/14793. Bathroom Stalls （Small2）/Bathroom Stalls （Small2）.java
@@ -4,75 +4,57 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+
+        long[] arr = new long[63];
+        arr[0] = 1;
+
+        for (int i = 1; i < 63; i++) {
+            arr[i] = 2*arr[i-1];
+        }
 
         int T = Integer.parseInt(br.readLine());
 
         for (int tc = 1; tc <= T; tc++) {
             st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-            PriorityQueue<Node> q = new PriorityQueue<>();
-            q.add(new Node(0, N + 1));
-
-            int ls = 0;
-            int rs = 0;
-
-            for (int i = 0; i < K; i++) {
-                Node node = q.poll();
-                int l = node.getL();
-                int r = node.getR();
-                int mid = (l + r) / 2;
-
-                ls = mid - l - 1;
-                rs = r - mid - 1;
-                if (mid - l != 1) {
-                    q.add(new Node(l, mid));
+            long N = Long.parseLong(st.nextToken());
+            long K = Long.parseLong(st.nextToken());
+            K++;
+            int n = 0;
+            for (int i = 62; i >= 1; i--) {
+                if (K >= arr[i]) {
+                    n = i;
+                    break;
                 }
-                if (r - mid != 1) {
-                    q.add(new Node(mid, r));
+            }
+            K--;
+            N -= arr[n] - 1;
+            K -= arr[n] - 1;
+
+            long q = N / arr[n];
+            long r = N % arr[n];
+
+            long ls;
+            long rs;
+            if (K != 0) {
+                if (K <= r) {
+                    q++;
+                }
+                q--;
+                ls = q/2 + q%2;
+                rs = q/2;
+            }
+            else {
+                ls = q;
+                rs = q;
+                if (r != 0) {
+                    if (arr[n] / 2 <= r) {
+                        ls++;
+                    }
                 }
             }
 
-            System.out.println("Case #" + tc + ": " + Math.max(ls, rs) + " " + Math.min(ls, rs));
+            System.out.println("Case #" + tc + ": " + ls + " " + rs);
         }
-    }
-}
-
-class Node implements Comparable<Node> {
-    private int l;
-    private int r;
-    private int dis;
-
-    public Node(int l, int r) {
-        this.l = l;
-        this.r = r;
-        this.dis = r - l;
-    }
-
-    public int getL() {
-        return this.l;
-    }
-
-    public int getR() {
-        return this.r;
-    }
-
-    public int getDis() {
-        return this.dis;
-    }
-
-    @Override
-    public int compareTo(Node node) {
-        if (this.dis < node.dis) {
-            return 1;
-        }
-        else if (this.dis == node.dis) {
-            if (this.l > node.l) {
-                return 1;
-            }
-        }
-        return -1;
     }
 }
