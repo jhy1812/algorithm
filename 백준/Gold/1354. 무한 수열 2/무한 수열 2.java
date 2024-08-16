2d2ffc996dee144454c3339418338d7d72584729
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2024-08-16 21:39:05 +0900

    [Gold V] Title: 무한 수열 2, Time: 4704 ms, Memory: 796140 KB -BaekjoonHub

---
+++ 백준/Gold/1354. 무한 수열 2/무한 수열 2.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static long N, P, Q, X, Y;
+    public static HashMap<Long, Long> dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Long.parseLong(st.nextToken());
+        P = Long.parseLong(st.nextToken());
+        Q = Long.parseLong(st.nextToken());
+        X = Long.parseLong(st.nextToken());
+        Y = Long.parseLong(st.nextToken());
+
+        dp = new HashMap<>();
+
+        System.out.print(dfs(N));
+    }
+
+    public static long dfs(long n) {
+        if (n <= 0) {
+            return 1L;
+        }
+
+        if (dp.containsKey(n)) {
+            return dp.get(n);
+        }
+
+        long ret = dfs(n/P - X) + dfs(n/Q - Y);
+        dp.put(n, ret);
+
+        return ret;
+    }
+}
