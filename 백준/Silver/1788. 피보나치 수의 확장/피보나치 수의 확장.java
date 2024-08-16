53cbef813a2f1169bbbc7648745b990ff54d0f91
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2024-08-16 21:13:45 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 140 ms, Memory: 16084 KB -BaekjoonHub

--- 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.java
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.java
@@ -15,9 +15,7 @@ public class Main {
         dp[1] = 1;
 
         for (int i = 2; i <= Math.abs(n); i++) {
-            dp[i%3] = dp[(i+1)%3] % MOD;
-            dp[i%3] += dp[(i+2)%3] % MOD;
-            dp[i%3] %= MOD;
+            dp[i%3] = (dp[(i+1)%3] + dp[(i+2)%3]) % MOD;
         }
 
         if (n < 0 && Math.abs(n)%2 == 0) {
