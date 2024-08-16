9eaed9c3d56620ea4577b420ad48946af93359c3
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2024-08-16 21:12:01 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 140 ms, Memory: 16144 KB -BaekjoonHub

---
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int MOD = 1000000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int sign = 0;
+
+        int[] dp = new int[3];
+        dp[1] = 1;
+
+        for (int i = 2; i <= Math.abs(n); i++) {
+            dp[i%3] = dp[(i+1)%3] % MOD;
+            dp[i%3] += dp[(i+2)%3] % MOD;
+            dp[i%3] %= MOD;
+        }
+
+        if (n < 0 && Math.abs(n)%2 == 0) {
+            sign = -1;
+        }
+        else if (n != 0) {
+            sign = 1;
+        }
+
+        System.out.print(sign + "\n" + dp[Math.abs(n)%3]);
+    }
+}
