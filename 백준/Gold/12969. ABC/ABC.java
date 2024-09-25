581debe71c0fb17b58afc4710bec8f874a69a1fc
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2024-09-26 03:55:04 +0900

    [Gold I] Title: ABC, Time: 144 ms, Memory: 29440 KB -BaekjoonHub

--- 백준/Gold/12969. ABC/ABC.java
+++ 백준/Gold/12969. ABC/ABC.java
@@ -5,7 +5,8 @@ public class Main {
 
     public static boolean check = false;
     public static int N, K;
-    public static String result;
+    public static char[] ret;
+    public static StringBuilder result;
     public static boolean[][][][] dp;
 
     public static void main(String[] args)throws IOException {
@@ -14,31 +15,37 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        result = "";
+        result = new StringBuilder();
+        ret = new char[N];
         dp = new boolean[N+1][N+1][N+1][N*(N-1)/2+1];
 
-        dfs(0, 0, 0, 0, "");
+        dfs(0, 0, 0, 0);
 
         if (check) {
-            System.out.print(result);
+            System.out.print(result.toString());
         }
         else {
             System.out.print(-1);
         }
     }
 
-    public static void dfs(int lv, int a, int b, int p, String s) {
+    public static void dfs(int lv, int a, int b, int p) {
         if (check || dp[lv][a][b][p]) return;
         if (lv == N) {
             if (p == K) {
-                result = s;
+                for (int i = 0; i < N; i++) {
+                    result.append(ret[i]);
+                }
                 check = true;
             }
             return;
         }
         dp[lv][a][b][p] = true;
-        dfs(lv+1, a+1, b, p, s.substring(0, lv)+"A");
-        dfs(lv+1, a, b+1, p+a, s.substring(0, lv)+"B");
-        dfs(lv+1, a, b, p+a+b, s.substring(0, lv)+"C");
+        ret[lv] = 'A';
+        dfs(lv+1, a+1, b, p);
+        ret[lv] = 'B';
+        dfs(lv+1, a, b+1, p+a);
+        ret[lv] = 'C';
+        dfs(lv+1, a, b, p+a+b);
     }
 }
