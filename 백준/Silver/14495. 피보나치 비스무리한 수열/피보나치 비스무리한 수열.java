faab0d5c8966018f1df377ebe54f82de1defa8d8
Jung-Ho-Yun <122588619+jhy1812@users.noreply.github.com>
2024-11-24 21:08:02 +0900

    [Silver IV] Title: 피보나치 비스무리한 수열, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/14495. 피보나치 비스무리한 수열/피보나치 비스무리한 수열.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+        if (n <= 3) {
+            System.out.print(1);
+        }
+        else {
+            long[] dp = new long[n+1];
+            dp[1] = 1L;
+            dp[2] = 1L;
+            dp[3] = 1L;
+
+            for (int i = 4; i <= n;i++) {
+                dp[i] = dp[i-1] + dp[i-3];
+            }
+
+            System.out.print(dp[n]);
+        }
+    }
+}
